d77da8c738cf9de3821f8f267a8ec4ccbeacfb19
Prsmiami <Prsmiami@users.noreply.github.com>
2019-03-25 01:53:28 -0400

    Room functionality
    Random room working, custom room throwing error

--- Client server python files/Checkers Files/CheckersClient.py
+++ Client server python files/Checkers Files/CheckersClient.py
@@ -19,37 +19,9 @@ def Application(csoc):
     ##Input move or wait for recieve board
     ##
 
-
+    print(loopRecv(csoc,8).decode())
     
-    size = 1000
-    while (size > 999):
-        rawmessage = input()
-        if(rawmessage == "leave"):
-            exit()
-        message = str(datetime.datetime.now())[11:-7]
-        message += "#"
-        message += alias
-        message += "#"
-        message += rawmessage
-        size = len(message)
-        if(size > 999):
-            print("message too large")
 
-    # make sure size is 4 bytes
-    if(size > 99): size = (str(size),":")
-    elif(size > 9): size = ("0",str(size),":")
-    else: size = ("00",str(size),":")
-
-    # send size to server
-    for e in size:
-        csoc.sendall(e.encode("utf-8"))
-    #csoc.sendall(size.encode())
-
-    # send message to server
-    for e in message:
-        csoc.sendall(e.encode("utf-8"))
-    #csoc.sendall(message.encode())
-        
 
 if __name__ == "__main__":
     # create the socket
@@ -61,22 +33,22 @@ if __name__ == "__main__":
 
     print("Started Application")
 
-    while(true):
+    while(True):
         print("Please type either 'random' for a random opponent or a 6 digit alphanumeric room code for a match with a friend.")
-
+        roomtype = ""
         roomtype = input()
 
-        print("You typed: ",roomtype," , is this correct?");
+        print("You typed: ",roomtype,", is this correct?");
         confirmation = input();
-        if(len(roomtype) = 6):
-            if(confirmation = ("y" || "Y" || "yes" || "Yes"): break;
-        else: print("Must enter either 'random' or a 6 digit alphanumeric code");
+        if(len(roomtype) == 6):
+            if( confirmation == "y" or confirmation == "Y" or confirmation == "yes" or confirmation == "Yes"): break
+        else: print("Must enter either 'random' or a 6 digit alphanumeric code")
 
-    if(roomtype = "random" || "Random"):
-        csoc.sendall(("R").encode("utf-8"));
+    if(roomtype == "random" or roomtype == "Random"):
+        commsoc.sendall(("R").encode("utf-8"));
     else:
-        csoc.sendall(("C").encode("utf-8"));
-        csoc.sendall(roomtype);
+        commsoc.sendall(("C").encode("utf-8"));
+        commsoc.sendall((roomtype).encode("utf-8"));
     
     # run the application protocol    
     Application(commsoc)
--- Client server python files/Checkers Files/CheckersServer.py
+++ Client server python files/Checkers Files/CheckersServer.py
@@ -1,5 +1,14 @@
 import selectors
 import socket
+import threading
+
+class waitinguser:
+    socket = 0
+    roomcode = 0
+
+    def make_waitinguser(var1,var2):
+        socket = var1
+        roomcode = var2
 
 def doAccept(thesel, thesock, mask):
     # accept the connection
@@ -10,28 +19,62 @@ def doAccept(thesel, thesock, mask):
     # register
     thesel.register(commsoc, selectors.EVENT_READ, doRead)
 
-def doRead(thesel, thesock, mask):
-    data = thesock.recv(1024)
-    esoc = socket.socket(type=socket.SOCK_DGRAM)
-    mc_addr = "224.0.0.70"
-    mc_port = 50000
-    esoc.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 1)
-    lhost = socket.gethostbyname("localhost")
-    esoc.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_IF, socket.inet_aton(lhost))
+def Roomhandler(sock1, sock2, mask):
+    
+    ##insert function here
+    sock1.sendall(("socket 1").encode("utf-8"))
+    sock2.sendall(("socket 2").encode("utf-8"))
+    
 
+def doRead(thesel, thesock, mask):
+    data = thesock.recv(1)
+    global randomflag
+    global randomopponent
+    global waitlist
+    deleteflag = 0
     if data:
         print("recv:", data)
-        esoc.sendto(data,(mc_addr, mc_port))
-    else:
-        print("closing", thesock)
-        thesel.unregister(thesock)
-        thesock.close()
+
+    ##code for random room    
+        if(data.decode() == 'R'):
+            if(randomflag == 0):
+                randomopponent = thesock
+                randomflag = 1
+            else:
+                t1 = threading.Thread(name="first", target=Roomhandler, args=(randomopponent,thesock,mask))
+                t1.start()
+                thesel.unregister(randomopponent)
+                thesel.unregister(thesock)
+
+    ##code for specified room number
+        if(data.decode() == 'C'):
+            data = (thesock.recv(6)).decode()
+            print("Room number: ",data)
+            for e in waitlist:
+                if(waitlist[e].roomcode == data):
+                    t1 = threading.Thread(name="first", target=Roomhandler, args=(e.socket,thesock,mask))
+                    t1.start()
+                    thesel.unregister(e.sock)
+                    thesel.unregister(thesock)
+                    delindex = e
+                    deleteflag = 1
+                    break
+            if(deleteflag == 1):
+                del waitlist[delindex]
+            else:
+                i = len(waitlist)
+                waitlist[i] = waitinguser.make_waitinguser(thesock,data)
+
 
 if __name__ == "__main__":
+
+    randomflag = 0
+    threadnum = 0
+    randomopponent = 0
+    waitlist = [100]
     
     # create the selector
     sel = selectors.DefaultSelector()
-    
     
     # create the server socket
     #  defaults family=AF_INET, type=SOCK_STREAM, proto=0, filno=None
@@ -43,15 +86,14 @@ if __name__ == "__main__":
     # bind to local host:5000
     serversoc.bind(("localhost",50000))
                    
-    # make passive with backlog=5
-    serversoc.listen(5)
+    # make passive with backlog=10
+    serversoc.listen(10)
     
     # register the server socket
     sel.register(serversoc, selectors.EVENT_READ, doAccept)
     
     # wait for incoming connections
     while True:
-        print("Listening on ", 50000)
         
         events = sel.select()
         for key, mask in events:
