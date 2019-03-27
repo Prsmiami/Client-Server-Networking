6893d6d560d62141594b42e90560a47b80139392
bxs388 <48105289+bxs388@users.noreply.github.com>
2019-03-27 03:27:03 -0400

    board set up

--- Client server python files/Checkers Files/CheckersClient.py
+++ Client server python files/Checkers Files/CheckersClient.py
@@ -1,6 +1,22 @@
 import socket
 import datetime
+import stddraw
+import sys
 
+def printcheckboard(): 
+    n = 8
+    stddraw.setXscale(0, n)
+    stddraw.setYscale(0, n)
+    
+    for i in range(n):
+        for j in range(n):
+            if ((i + j) % 2) != 0:
+                stddraw.setPenColor(stddraw.BLACK)
+            else:
+                stddraw.setPenColor(stddraw.RED)
+            stddraw.filledSquare(i + .5, j + .5, .5)
+    stddraw.show()
+  
 
 def loopRecv(csoc, size):
     data = bytearray(b" "*size)
@@ -21,6 +37,9 @@ def Application(csoc):
     ##
 
     print(loopRecv(csoc,8).decode())
+    # driver code 
+    printcheckboard() 
+    
     
 
 
