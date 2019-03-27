950791f1e1a656f9c87a0ecae967c97732f89e20
Prsmiami <Prsmiami@users.noreply.github.com>
2019-03-27 03:19:45 -0400

    Server move decode configured

--- Client server python files/Checkers Files/CheckersServer.py
+++ Client server python files/Checkers Files/CheckersServer.py
@@ -2,6 +2,15 @@ import selectors
 import socket
 import threading
 
+def loopRecv(csoc, size):
+    data = bytearray(b" "*size)
+    mv = memoryview(data)
+    while size:
+        rsize = csoc.recv_into(mv,size)
+        mv = mv[rsize:]
+        size -= rsize
+    return data
+
 class waitinguser:
     prevuser = 0
     socket = 0
@@ -25,9 +34,166 @@ def doAccept(thesel, thesock, mask):
 
 def Roomhandler(sock1, sock2, mask):
     
-    ##insert function here
     sock1.sendall(("socket 1").encode("utf-8"))
     sock2.sendall(("socket 2").encode("utf-8"))
+##  turn 1 is for socket 1
+    turn = 1
+    endgame = 0
+    mult = 0
+    base = 0
+    fromindex, toindex = 0
+    move = ""
+    board = [1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,2,2,2,2,2,2,2,2,2,2,2,2]
+    while(endgame != 1):
+        if(turn == 1):
+            sock1.sendall(("T").encode("utf-8"))
+            sock2.sendall(("N").encode("utf-8"))
+            for e in board:
+                sock1.sendall((e).encode("utf-8"))
+                sock2.sendall((e).encode("utf-8"))
+            move = (loopRecv(sock1,4)).decode()
+            if(move[0] == 'A'):
+                mult = 0
+            else if(move[0] == 'B'):
+                mult = 1
+            else if(move[0] == 'C'):
+                mult = 2
+            else if(move[0] == 'D'):
+                mult = 3
+            else if(move[0] == 'E'):
+                mult = 4
+            else if(move[0] == 'F'):
+                mult = 5
+            else if(move[0] == 'G'):
+                mult = 6
+            else if(move[0] == 'H'):
+                mult = 7
+            if(move[1] == '1'):
+                base = 0
+            else if(move[1] == '2'):
+                base = 0
+            else if(move[1] == '3'):
+                base = 1
+            else if(move[1] == '4'):
+                base = 1
+            else if(move[1] == '5'):
+                base = 2
+            else if(move[1] == '6'):
+                base = 2
+            else if(move[1] == '7'):
+                base = 3
+            else if(move[1] == '8'):
+                base = 3
+            fromindex = base + (mult*4)
+
+            if(move[2] == 'A'):
+                mult = 0
+            else if(move[2] == 'B'):
+                mult = 1
+            else if(move[2] == 'C'):
+                mult = 2
+            else if(move[2] == 'D'):
+                mult = 3
+            else if(move[2] == 'E'):
+                mult = 4
+            else if(move[2] == 'F'):
+                mult = 5
+            else if(move[2] == 'G'):
+                mult = 6
+            else if(move[2] == 'H'):
+                mult = 7
+            if(move[3] == '1'):
+                base = 0
+            else if(move[3] == '2'):
+                base = 0
+            else if(move[3] == '3'):
+                base = 1
+            else if(move[3] == '4'):
+                base = 1
+            else if(move[3] == '5'):
+                base = 2
+            else if(move[3] == '6'):
+                base = 2
+            else if(move[3] == '7'):
+                base = 3
+            else if(move[3] == '8'):
+                base = 3
+            toindex = base + (mult*4)
+            
+        if(turn == 2):
+            sock2.sendall(("T").encode("utf-8"))
+            sock1.sendall(("N").encode("utf-8"))
+            for e in board:
+                sock1.sendall((e).encode("utf-8"))
+                sock2.sendall((e).encode("utf-8"))
+            move = (loopRecv(sock1,4)).decode()
+            if(move[0] == 'A'):
+                mult = 0
+            else if(move[0] == 'B'):
+                mult = 1
+            else if(move[0] == 'C'):
+                mult = 2
+            else if(move[0] == 'D'):
+                mult = 3
+            else if(move[0] == 'E'):
+                mult = 4
+            else if(move[0] == 'F'):
+                mult = 5
+            else if(move[0] == 'G'):
+                mult = 6
+            else if(move[0] == 'H'):
+                mult = 7
+            if(move[1] == '1'):
+                base = 0
+            else if(move[1] == '2'):
+                base = 0
+            else if(move[1] == '3'):
+                base = 1
+            else if(move[1] == '4'):
+                base = 1
+            else if(move[1] == '5'):
+                base = 2
+            else if(move[1] == '6'):
+                base = 2
+            else if(move[1] == '7'):
+                base = 3
+            else if(move[1] == '8'):
+                base = 3
+            fromindex = base + (mult*4)
+
+            if(move[2] == 'A'):
+                mult = 0
+            else if(move[2] == 'B'):
+                mult = 1
+            else if(move[2] == 'C'):
+                mult = 2
+            else if(move[2] == 'D'):
+                mult = 3
+            else if(move[2] == 'E'):
+                mult = 4
+            else if(move[2] == 'F'):
+                mult = 5
+            else if(move[2] == 'G'):
+                mult = 6
+            else if(move[2] == 'H'):
+                mult = 7
+            if(move[3] == '1'):
+                base = 0
+            else if(move[3] == '2'):
+                base = 0
+            else if(move[3] == '3'):
+                base = 1
+            else if(move[3] == '4'):
+                base = 1
+            else if(move[3] == '5'):
+                base = 2
+            else if(move[3] == '6'):
+                base = 2
+            else if(move[3] == '7'):
+                base = 3
+            else if(move[3] == '8'):
+                base = 3
+            toindex = base + (mult*4)            
     
 
 def doRead(thesel, thesock, mask):
