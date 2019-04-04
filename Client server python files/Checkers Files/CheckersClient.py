0209c979414474b6934e4fe9ff83d5ebbe6d00ed
bxs388 <48105289+bxs388@users.noreply.github.com>
2019-04-04 19:03:37 -0400

    working board w labels on tiles

--- Client server python files/Checkers Files/CheckersClient.py
+++ Client server python files/Checkers Files/CheckersClient.py
@@ -24,103 +24,107 @@ def printcheckboard(values):
             else:
                 stddraw.setPenColor(stddraw.RED)
             stddraw.filledSquare(j + .5, i + .5, .5)
+            stddraw.setPenColor(stddraw.YELLOW)
+            location = rows[j] + cols[i]
+            stddraw.text(j+.15, i+.1, location)
             if((i + j) % 2) == 0:
                 if(values[z] == '1'):           ##player 1
                     stddraw.setPenColor(stddraw.BLUE)
+                    stddraw.filledCircle(j + .5, i + .5, .25)
                 elif(values[z] == '2'):         ##player 2
                     stddraw.setPenColor(stddraw.WHITE)
-                stddraw.filledCircle(j + .5, i + .5, .25)
+                    stddraw.filledCircle(j + .5, i + .5, .25)
                 z+=1
     stddraw.show()
 
-def isvalid(board, move):
-
-    difference = 0
-
-    if(move[0] == 'A'):
-        mult = 0
-    elif(move[0] == 'B'):
-        mult = 1
-    elif(move[0] == 'C'):
-        mult = 2
-    elif(move[0] == 'D'):
-        mult = 3
-    elif(move[0] == 'E'):
-        mult = 4
-    elif(move[0] == 'F'):
-        mult = 5
-    elif(move[0] == 'G'):
-        mult = 6
-    elif(move[0] == 'H'):
-        mult = 7
-    if(move[1] == '1'):
-        base = 0
-    elif(move[1] == '2'):
-        base = 0
-    elif(move[1] == '3'):
-        base = 1
-    elif(move[1] == '4'):
-        base = 1
-    elif(move[1] == '5'):
-        base = 2
-    elif(move[1] == '6'):
-        base = 2
-    elif(move[1] == '7'):
-        base = 3
-    elif(move[1] == '8'):
-        base = 3
-    fromindex = base + (mult*4)
-
-    if(move[2] == 'A'):
-        mult = 0
-    elif(move[2] == 'B'):
-        mult = 1
-    elif(move[2] == 'C'):
-        mult = 2
-    elif(move[2] == 'D'):
-        mult = 3
-    elif(move[2] == 'E'):
-        mult = 4
-    elif(move[2] == 'F'):
-        mult = 5
-    elif(move[2] == 'G'):
-        mult = 6
-    elif(move[2] == 'H'):
-        mult = 7
-    if(move[3] == '1'):
-        base = 0
-    elif(move[3] == '2'):
-        base = 0
-    elif(move[3] == '3'):
-        base = 1
-    elif(move[3] == '4'):
-        base = 1
-    elif(move[3] == '5'):
-        base = 2
-    elif(move[3] == '6'):
-        base = 2
-    elif(move[3] == '7'):
-        base = 3
-    elif(move[3] == '8'):
-        base = 3
-    toindex = base + (mult*4)
-
-    if((move[0] == (move[2]-1)) and move[0] != H):
-        if(board[fromindex] == 1):
-            if(board[toindex] == 0):
-                board[fromindex] = 0
-                board[toindex] = 1
-            else: print("Please select a space that is not occupied by another piece.")
-        else: print("Please select a piece that is yours. Your pieces are on the bottom.")
-    elif((move[0] == (move[2]-2)) and (move[0] != H) and (move[0] != G)):
-        if(board[fromindex] == 1):
-            difference = 0
-            if(board[toindex] == 0):
-                if(
-                else(
-
-        else: print("Please select a piece that is yours. Your pieces are on the bottom")
-  
+##def isvalid(board, move):
+##
+##    difference = 0
+##
+##    if(move[0] == 'A'):
+##        mult = 0
+##    elif(move[0] == 'B'):
+##        mult = 1
+##    elif(move[0] == 'C'):
+##        mult = 2
+##    elif(move[0] == 'D'):
+##        mult = 3
+##    elif(move[0] == 'E'):
+##        mult = 4
+##    elif(move[0] == 'F'):
+##        mult = 5
+##    elif(move[0] == 'G'):
+##        mult = 6
+##    elif(move[0] == 'H'):
+##        mult = 7
+##    if(move[1] == '1'):
+##        base = 0
+##    elif(move[1] == '2'):
+##        base = 0
+##    elif(move[1] == '3'):
+##        base = 1
+##    elif(move[1] == '4'):
+##        base = 1
+##    elif(move[1] == '5'):
+##        base = 2
+##    elif(move[1] == '6'):
+##        base = 2
+##    elif(move[1] == '7'):
+##        base = 3
+##    elif(move[1] == '8'):
+##        base = 3
+##    fromindex = base + (mult*4)
+##
+##    if(move[2] == 'A'):
+##        mult = 0
+##    elif(move[2] == 'B'):
+##        mult = 1
+##    elif(move[2] == 'C'):
+##        mult = 2
+##    elif(move[2] == 'D'):
+##        mult = 3
+##    elif(move[2] == 'E'):
+##        mult = 4
+##    elif(move[2] == 'F'):
+##        mult = 5
+##    elif(move[2] == 'G'):
+##        mult = 6
+##    elif(move[2] == 'H'):
+##        mult = 7
+##    if(move[3] == '1'):
+##        base = 0
+##    elif(move[3] == '2'):
+##        base = 0
+##    elif(move[3] == '3'):
+##        base = 1
+##    elif(move[3] == '4'):
+##        base = 1
+##    elif(move[3] == '5'):
+##        base = 2
+##    elif(move[3] == '6'):
+##        base = 2
+##    elif(move[3] == '7'):
+##        base = 3
+##    elif(move[3] == '8'):
+##        base = 3
+##    toindex = base + (mult*4)
+##
+##    if((move[0] == (move[2]-1)) and move[0] != H):
+##        if(board[fromindex] == 1):
+##            if(board[toindex] == 0):
+##                board[fromindex] = 0
+##                board[toindex] = 1
+##            else: print("Please select a space that is not occupied by another piece.")
+##        else: print("Please select a piece that is yours. Your pieces are on the bottom.")
+##    elif((move[0] == (move[2]-2)) and (move[0] != H) and (move[0] != G)):
+##        if(board[fromindex] == 1):
+##            difference = 0
+##            if(board[toindex] == 0):
+##                if(
+##                else(
+##
+##        else: print("Please select a piece that is yours. Your pieces are on the bottom")
+##  
 
 def loopRecv(csoc, size):
     data = bytearray(b" "*size)
@@ -149,24 +153,21 @@ def Application(csoc):
     board = loopRecv(csoc,32).decode()
     
     print(board)
-<<<<<<< HEAD
-    printcheckboard(board) 
-    
-=======
     printcheckboard(board)
-    assumeValid = 1;
+    assumeValid = 1
 
-    if(turn=='T')
+    if(turn=='T'):
         move = input()
-        while ((len(move) != 4) || !(assumeValid))    #(isvalid(move))
-            if(len(move) != 4)
+        while ((len(move) != 4) or not (assumeValid)):    #(isvalid(move))
+            if(len(move) != 4):
                 print("Please enter a valid 4 character move in the form of \n[currentrow][currentcolumn][nextrow][nextcolumn] \nfor example: C1D2")
-            if(!(isValid))
-                print("Please enter a valid move, your piece cant move there!"
->>>>>>> 3db16b9a82d246278842efdedd566506889c2a92
-    
+            if(not(assumeValid)):                      #(isvalid(move))
+                print("Please enter a valid move, your piece cant move there!")
+            move = input()
+        
 
 
+            
 if __name__ == "__main__":
     # create the socket
     #  defaults family=AF_INET, type=SOCK_STREAM, proto=0, filno=None
